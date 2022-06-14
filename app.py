4dec0b49eaf94514430018137055cd28d693fb65
Patrick Sontag <pmsontag84@gmail.com>
2022-06-14 15:04:26 -0500

    templates added

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from random import choice, sample
+# from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """homepage"""
+    return render_template("home.html")
+
+@app.route('/form')
+def form():
+    """show form for story inputs"""
+    return render_template("form.html")
+
+@app.route('/story')
+def story():
+    """show completed story with user inputs"""
+    return render_template("story.html")
+
