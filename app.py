6629a990de70371c53dec19f1928f30f4155f7e6
Patrick Sontag <pmsontag84@gmail.com>
2022-06-16 08:35:39 -0500

    updated

--- app.py
+++ app.py
@@ -1,43 +1,69 @@
-from flask import Flask, request, render_template
+# from flask import Flask, request, render_template
+# from flask_debugtoolbar import DebugToolbarExtension
+# from random import choice, sample
+# from stories import story
+
+# app = Flask(__name__)
+
+# app.config['SECRET_KEY'] = "secret"
+# debug = DebugToolbarExtension(app)
+
+
+# # @app.route('/')
+# # def home():
+# #     """homepage"""
+# #     return render_template("home.html")
+
+# @app.route('/')
+# def form():
+#     """show form for story inputs"""
+#     prompts = story.prompts
+
+#     return render_template("form.html", prompts=prompts)
+
+# @app.route('/story')
+# def story():
+#     """show completed story with user inputs"""
+#     # wants = request.args.get("comps")
+
+#     # if wants:
+#     #     add_words = sample(COMPLIMENTS, 1)
+
+#     v = request.args["verb"]
+#     n = request.args["noun"]
+#     a = request.args["adjective"]
+#     n2 = request.args['noun2']
+
+#     ans = { "vb": v,
+#             "nou": n,
+#             "adj": a,
+#             "nou2": n2}
+
+#     return render_template("story.html", verb=v, noun=n, adjective=a, noun2=n2, answers=ans)
+
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
-from random import choice, sample
 from stories import story
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "secret"
+
 debug = DebugToolbarExtension(app)
 
 
-# @app.route('/')
-# def home():
-#     """homepage"""
-#     return render_template("home.html")
+@app.route("/")
+def ask_questions():
+    """Generate and show form to ask words."""
 
-@app.route('/')
-def form():
-    """show form for story inputs"""
     prompts = story.prompts
 
     return render_template("form.html", prompts=prompts)
 
-@app.route('/story')
-def story():
-    """show completed story with user inputs"""
-    # wants = request.args.get("comps")
 
-    # if wants:
-    #     add_words = sample(COMPLIMENTS, 1)
+@app.route("/story")
+def show_story():
+    """Show story result."""
 
-    v = request.args["verb"]
-    n = request.args["noun"]
-    a = request.args["adjective"]
-    n2 = request.args['noun2']
+    text = story.generate(request.args)
 
-    ans = { "vb": v,
-            "nou": n,
-            "adj": a,
-            "nou2": n2}
-
-    return render_template("story.html", verb=v, noun=n, adjective=a, noun2=n2, answers=ans)
-
+    return render_template("story.html", text=text)
